b82752f28b8c7bfc6d96c381680112251c3a1107
naamashiponi <naamashiponi11@gmail.com>
2024-01-22 00:11:26 +0200

    Disable debug mode and update app title

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'iBet',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
